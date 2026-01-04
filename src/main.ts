11a9c0407c51242e2fd6e0af52b01c5629055860
kawabata <kenjuly67@gmail.com>
2026-01-04 16:47:55 +0900

    updateex

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import { ex1 } from './ex1.ts';
 import { ex2 } from './ex2.ts';
-import { ex3 } from './es3.ts';
+import { ex3 } from './ex3.ts';
 import { ex4 } from './ex4.ts';
 import { ex5 } from './ex5.ts';
 ex1();
