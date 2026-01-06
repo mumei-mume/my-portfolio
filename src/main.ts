91a35f8a86bb88277b8333ac34086c6fb1175089
Kenta Kawabata <kenjuly67@gmail.com>
2026-01-06 21:59:13 +0900

    1月6日mの授業で行った内容をコミットします

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import './style.css'
-import { ex1 } from './ex1.ts';
+// import './style.css'
+// import { ex1 } from './ex1.ts';
 import { ex2 } from './ex2.ts';
-import { ex3 } from './ex3.ts';
-import { ex4 } from './ex4.ts';
-import { ex5 } from './ex5.ts';
-import { training } from './training.ts';
-ex1();
+// import { ex3 } from './ex3.ts';
+// import { ex4 } from './ex4.ts';
+// import { ex5 } from './ex5.ts';
+// import { training } from './training.ts';
+// ex1();
 ex2();
-ex3();
-ex4();
-ex5();
-training();
+// ex3();
+// ex4();
+// ex5();
+// training();
--- src/training.ts
+++ src/training.ts
@@ -1,3 +1,16 @@
 export function training () {
-  console.log(111111111);
+  // let a: number = 3;
+  // a++;
+  // console.log(a);
+  // ++a;
+  // console.log(a);
+
+let a = 3;
+let b: number;
+b = a++;
+console.log(a, b);
+
+a = 3;
+b = ++a;
+console.log(a, b);
 }
