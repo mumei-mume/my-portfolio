6cd33e5e294ac79e2502e62645993b29a1b9be18
Kenta Kawabata <kenjuly67@gmail.com>
2026-01-11 19:40:44 +0900

    API連携の練習をしました。

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 // import './style.css'
 // import { ex1 } from './ex1.ts';
-import { ex2 } from './ex2.ts';
+// import { ex2 } from './ex2.ts';
 // import { ex3 } from './ex3.ts';
 // import { ex4 } from './ex4.ts';
 // import { ex5 } from './ex5.ts';
 // import { training } from './training.ts';
+import { whether } from './whether.ts';
 // ex1();
-ex2();
+// ex2();
 // ex3();
 // ex4();
 // ex5();
 // training();
+whether();
---
+++ src/whether.ts
@@ -0,0 +1,103 @@
+import Chart from 'chart.js/auto';
+
+type WeatherResponse = {
+  daily: {
+    time: string[];
+    temperature_2m_max: number[];
+    temperature_2m_min: number[];
+  };
+};
+
+export function whether() {
+  const url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&hourly=temperature_2m&timezone=Asia%2FTokyo';
+
+fetch(url)
+  .then ((data) => {
+    return data.json();
+  })
+  .then ((json) => {
+    drawChart(json);
+  });
+
+  function drawChart(json: WeatherResponse): void {
+  const mydata = {
+    labels: json.daily.time,
+    datasets: [{
+      label: '最高気温',
+      data: json.daily.temperature_2m_max,
+      borderColor: 'rgb(192, 75, 75)',
+    },{
+      label: '最低気温',
+      data: json.daily.temperature_2m_min,
+      borderColor: 'rgb(75, 75, 192)',
+    }]
+  };
+  
+  const graph = document.getElementById('stage') as HTMLCanvasElement;
+  new Chart(graph, {
+    type: 'line',
+    data: mydata,
+  });
+}
+
+
+} 
+
+
+
+// 下記非同期処理の学習コメント用
+
+// setTimeout(() => {
+//   console.log(3);
+//   setTimeout(() => {
+//     console.log(2);
+//     setTimeout(() => {
+//       console.log(1);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+// console.log("スタート");
+
+// new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log(3);
+//     resolve();
+//   }, 1000);
+// } ).then(() => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(2);
+//       resolve();
+//     }, 1000);
+//   });
+// }).then(() => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       console.log(1);
+//       resolve();
+//     }, 1000);
+//   });
+// }); 
+
+// // new Promise(() => {}); 基本形
+
+// new Promise (resolve => resolve("こんにちは"))
+//   .then(res => console.log(res));
+
+// func = async () => {
+//   await log(3);
+//   await log(2);
+//   await log(1);
+// };
+
+// log = (num) => {
+//   return new Promise (resolve => {
+//     setTimeout(() => {
+//       console.log(num);
+//       resolve();
+//     }, 1000);
+//   });
+// };
+
+// func();
